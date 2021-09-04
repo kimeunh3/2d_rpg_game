1e5b3be7cc45f1a653d6698b00cba27e37340eb0
Eunhye Kim <eunhyekim1223@gmail.com>
2021-09-04 21:59:24 +0900

    added enemyAI

--- RPG_Game/Assets/Scripts/Enemy.cs
+++ RPG_Game/Assets/Scripts/Enemy.cs
@@ -5,50 +5,41 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject prfHpBar;
-    public GameObject canvas;
-
-    RectTransform hpBar;
-
-    // 체력바를 적의 머리 위에 위치시키기 위함
-    public float height = 1.7f;
-
     public string enemyName;
     public int maxHp;
     public int nowHp;
     public int atkDmg;
-    public int atkSpeed;
+    public float atkSpeed;
+    public float moveSpeed;
+    public float atkRange;
+    public float fieldOfVision;
 
-    private void SetEnemyStatus(string _enemyName, int _maxHp, int _atkDmg, int _atkSpeed)
+    private void SetEnemyStatus(string _enemyName, int _maxHp, 
+        int _atkDmg, float _atkSpeed, float _moveSpeed,
+        float _atkRange, float _fieldOfVision)
     {
         enemyName = _enemyName;
         maxHp = _maxHp;
         nowHp = _maxHp;
         atkDmg = _atkDmg;
         atkSpeed = _atkSpeed;
+        moveSpeed = _moveSpeed;
+        atkRange = _atkRange;
+        fieldOfVision = _fieldOfVision;
     }
     
+    public GameObject prfHpBar;
+    public GameObject canvas;
+
+    RectTransform hpBar;
+
+    // 체력바를 적의 머리 위에 위치시키기 위함
+    public float height = 2.7f;
+
     public Sword_Man sword_man;
     Image nowHpbar;
+    public Animator enemyAnimator;
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Player"))
-        {
-            if (sword_man.attacked)
-            {
-                nowHp -= sword_man.atkDmg;
-                Debug.Log(nowHp);
-                sword_man.attacked = false;
-                if (nowHp <= 0)
-                {
-                    Destroy(gameObject);
-                    Destroy(hpBar.gameObject);
-                }
-            }
-        }
-    }
-    // Start is called before the first frame update
     void Start()
     {
         // 체력바를 생성하되 canvas의 자식으로 생성하고, 체력바의 위치 변경을 쉽게 하기
@@ -61,19 +52,58 @@ public class Enemy : MonoBehaviour
         hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();
         if (name.Equals("Enemy1"))
         {
-            SetEnemyStatus("Enemy1", 100, 10, 1);
+            SetEnemyStatus("Enemy1", 100, 10, 1.5f, 2, 1.5f, 7f);
         }
         nowHpbar = hpBar.transform.GetChild(0).GetComponent<Image>();
+        
+        SetAttackSpeed(atkSpeed);
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Camera.main.WorldToScreenPoint(월드 좌표 값); 
         // 월드 좌표를 스크린 좌표, 즉 UI좌표로 바꿔주는 함수
-        Vector3 _hpBarPos = 
-            Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
+        Vector3 _hpBarPos = Camera.main.WorldToScreenPoint(
+            new Vector3(transform.position.x, transform.position.y + height, 0));
         hpBar.position = _hpBarPos;
         nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (sword_man.attacked)
+            {
+                nowHp -= sword_man.atkDmg;
+                sword_man.attacked = false;
+                // 몬스터 사망
+                if (nowHp <= 0)
+                {
+                    Die();
+                }
+            }
+        }
+    }
+
+    void Die()
+    {
+        // die 애니메이션 실행
+        enemyAnimator.SetTrigger("die");
+        // 추적 비활성화
+        GetComponent<EnemyAI>().enabled = false;
+        // 충돌체 비활성화
+        GetComponent<Collider2D>().enabled = false;
+        // 중력 비활성화
+        Destroy(GetComponent<Rigidbody2D>());
+        // 3초 후 제거
+        Destroy(gameObject, 3);
+        // 3초 후 체력 바 제거
+        Destroy(hpBar.gameObject, 3);
+    }
+
+    void SetAttackSpeed(float speed)
+    {
+        enemyAnimator.SetFloat("attackSpeed", speed);
     }
 }
---
+++ RPG_Game/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    float attackDelay;
+
+    Enemy enemy;
+    Animator enemyAnimator;
+    void Start()
+    {
+        // print("here");
+        enemy = GetComponent<Enemy>();
+        enemyAnimator = enemy.enemyAnimator;
+
+    }
+
+    
+    void Update()
+    {
+        attackDelay -= Time.deltaTime;
+        if (attackDelay < 0) attackDelay = 0;
+
+        // 타겟과 자신의 거리를 확인
+        float distance = Vector3.Distance(transform.position, target.position);
+        // 공격 딜레이(쿨타임)가 0일 때, 시야 범위안에 들어올 때
+        if (attackDelay == 0 && distance <= enemy.fieldOfVision)
+        {
+            print("here");
+            // 타겟 바라보기
+            FaceTarget();
+
+            // 공격 범위 안에 들어올 때 공격
+            if (distance <= enemy.atkRange)
+            {
+                AttackTarget();
+            }
+            else // 공격 애니메이션 실행 중이 아닐 때 추적
+            {
+                if (!enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                {
+                    MoveToTarget();
+                }
+            }
+        }
+        else // 시야 범위 밖에 있을 때 Idle 애니메이션으로 전환
+        {
+            enemyAnimator.SetBool("moving", false);
+        }
+    }
+
+    void MoveToTarget()
+    {
+        float dir = target.position.x - transform.position.x;
+        dir = (dir < 0) ? -1 : 1;
+        transform.Translate(new Vector2(dir, 0) * enemy.moveSpeed * Time.deltaTime);
+        enemyAnimator.SetBool("moving", true);
+    }
+
+    void FaceTarget()
+    {
+        // 타겟이 왼쪽에 있을 때
+        if (target.position.x - transform.position.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else // 타겟이 오른쪽에 있을 때
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    void AttackTarget()
+    {
+        target.GetComponent<Sword_Man>().nowHp -= enemy.atkDmg;
+        // 공격 애니메이션 실행
+        enemyAnimator.SetTrigger("attack");
+        // 딜레이 충전
+        attackDelay = enemy.atkSpeed;
+    }
+}
