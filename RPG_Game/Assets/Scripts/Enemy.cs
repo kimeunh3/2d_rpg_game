b3483144665013eb95e4b135f1623d838713df5c
Eunhye Kim <eunhyekim1223@gmail.com>
2021-09-04 19:57:31 +0900

    Made enemy's hp bar decreases when it got attacked by swordman and swordman can jump. Also added tiles like grounds, decorations and background.

--- RPG_Game/Assets/Scripts/Enemy.cs
+++ RPG_Game/Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,18 +10,70 @@ public class Enemy : MonoBehaviour
 
     RectTransform hpBar;
 
+    // 체력바를 적의 머리 위에 위치시키기 위함
     public float height = 1.7f;
+
+    public string enemyName;
+    public int maxHp;
+    public int nowHp;
+    public int atkDmg;
+    public int atkSpeed;
+
+    private void SetEnemyStatus(string _enemyName, int _maxHp, int _atkDmg, int _atkSpeed)
+    {
+        enemyName = _enemyName;
+        maxHp = _maxHp;
+        nowHp = _maxHp;
+        atkDmg = _atkDmg;
+        atkSpeed = _atkSpeed;
+    }
+    
+    public Sword_Man sword_man;
+    Image nowHpbar;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (sword_man.attacked)
+            {
+                nowHp -= sword_man.atkDmg;
+                Debug.Log(nowHp);
+                sword_man.attacked = false;
+                if (nowHp <= 0)
+                {
+                    Destroy(gameObject);
+                    Destroy(hpBar.gameObject);
+                }
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
+        // 체력바를 생성하되 canvas의 자식으로 생성하고, 체력바의 위치 변경을 쉽게 하기
+        // 위해 hpBar에 저장한다.
+        // Instantiate(게임오브젝트, 부모의 transform); 게임오브젝트를 생성하는 함수
+        // 체력바 오브젝트의 위치를 바꿔주기 위해 GetComponent로 RectRansform를 받아주었다.
+        // 체력바 오브젝트는 UI이기 때문에 transform.position이 아닌 RectRansform을 사용
+        // GetComponent<컴포넌트 이름>(); 게임 오브젝트에 붙어있는 컴포넌트를 받아올 수 있음
+        // 컴포넌트: RectTransform, Canvas Renderer, Image 등등 
         hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();
+        if (name.Equals("Enemy1"))
+        {
+            SetEnemyStatus("Enemy1", 100, 10, 1);
+        }
+        nowHpbar = hpBar.transform.GetChild(0).GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Camera.main.WorldToScreenPoint(월드 좌표 값); 
+        // 월드 좌표를 스크린 좌표, 즉 UI좌표로 바꿔주는 함수
         Vector3 _hpBarPos = 
             Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
         hpBar.position = _hpBarPos;
+        nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
     }
 }
--- RPG_Game/Assets/Scripts/Sword_Man.cs
+++ RPG_Game/Assets/Scripts/Sword_Man.cs
@@ -1,47 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sword_Man : MonoBehaviour
 {
     Animator animator;
+    public int maxHp;
+    public int nowHp;
+    public int atkDmg;
+    public float atkSpeed = 1;
+    // 여러번 공격 방지용
+    public bool attacked = false;
+    public Image nowHpbar;
+
+    bool inputRight = false;
+    bool inputLeft = false;
+    Rigidbody2D rigid2D;
+    public float moveSpeed = 5;
+
+    BoxCollider2D col2D;
+
+    public float jumpPower = 40;
+    bool inputJump = false;
+
+    void AttackTrue()
+    {
+        attacked = true;
+    }
+    void AttackFalse()
+    {
+        attacked = false;
+    }
+    void SetAttackSpeed(float speed)
+    {
+        animator.SetFloat("attackSpeed", speed);
+        atkSpeed = speed;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        maxHp = 50;
+        nowHp = 50;
+        atkDmg = 10;
         // 오브젝트의 위치 초기화
         transform.position = new Vector3(0,0,0);
         // 오브젝트의 Animator 데이터를 받아옴
         animator = GetComponent<Animator>();
+        rigid2D = GetComponent<Rigidbody2D>();
+
+        SetAttackSpeed(1.5f);
+        col2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
         // 좌우 방향키에 따른 값 받기        
         if (Input.GetKey(KeyCode.RightArrow)) 
         {
+            inputRight = true;
             // 이동하는 방향에 따라 좌우반전
             transform.localScale = new Vector3(-1, 1, 1);
             // animator 상태 연결
             animator.SetBool("moving", true);
-            // 방향 값에 따라 움직이게 하기
-            transform.Translate(Vector3.right * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.LeftArrow)) 
         {
+            inputLeft = true;
             transform.localScale = new Vector3(1, 1, 1);
             animator.SetBool("moving", true);
-            transform.Translate(Vector3.left * Time.deltaTime);
         }
         else animator.SetBool("moving", false);
         
         // A키를 누르면 true 반환
         // "Attack" 애니메이션이 실행되고 있다면 true 반환
-        if (Input.GetKeyDown(KeyCode.A) && 
+        if (Input.GetKey(KeyCode.A) && 
             !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
         {
             // "attack" 트리거 실행
             animator.SetTrigger("attack");
+        }
+        // Raycast로 땅과 충돌 체크
+        RaycastHit2D raycastHit = 
+            Physics2D.BoxCast(col2D.bounds.center, col2D.bounds.size, 
+            0f, Vector2.down, 0.02f, LayerMask.GetMask("Ground"));
+        if (raycastHit.collider != null)
+            animator.SetBool("jumping", false);
+        else animator.SetBool("jumping", true);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !animator.GetBool("jumping"))
+        {
+            inputJump = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (inputRight)
+        {
+            inputRight = false;
+            rigid2D.AddForce(Vector2.right * moveSpeed);
+        }
+        if (inputLeft)
+        {
+            inputLeft = false;
+            rigid2D.AddForce(Vector2.left * moveSpeed);
+        }
+
+        // 속도 제한
+        if (rigid2D.velocity.x >= 2.5f) rigid2D.velocity = new Vector2(2.5f, rigid2D.velocity.y);
+        else if (rigid2D.velocity.x <= -2.5f) rigid2D.velocity = new Vector2(-2.5f, rigid2D.velocity.y);
+        
+        // 점프 구현
+        if(inputJump)
+        {
+            inputJump = false;
+            rigid2D.AddForce(Vector2.up * jumpPower);
         }
     }
 }
